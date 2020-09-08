3bee5fdf64ef7303ee1d4e39eabf641a76b781a0
Julia <shaow.1230@gmail.com>
2020-09-09 02:08:50 +0300

    added bar chart

--- js/app.js
+++ js/app.js
@@ -1,24 +1,77 @@
-var trafficChart = document.getElementById('traffic-chart');
-var lineChart = new Chart(trafficChart, {
+
+var trafficLineChart = document.getElementById('traffic-chart');
+var trafficBarChart = document.getElementById('daily-chart');
+var mobileUsersChart = document.getElementById('mobile-users-chart');
+
+//--------------- LINE CHART ---------------//
+//------------------------------------------//
+var lineChart = new Chart(trafficLineChart, {
     type: 'line',
     data: {
-        labels: ['16-20', '23-29', '30-5', '6-12', '13-19', '20-26', '27-3', '4-10', '11-17', '18-24', '25-31'],
         datasets: [{
-            label: '',
             lineTension: 0,
-            data: [12, 19, 3, 5, 2, 3],
-            backgroundColor: 'rgba(116,120,191,0.3)',
-            borderColor: 'rgba(116,120,191,0.7)',
+            data: [0, 750, 1250, 1000, 1500, 2000, 1500, 1750, 1250, 1750, 2250, 1750, 2250],
+            backgroundColor: 'rgba(115,119,191,0.2)',
+            borderColor: 'rgba(115,119,191,0.7)',
+            pointRadius: 6,
+            pointBackgroundColor: 'rgba(255,255,255,1)',
+            pointBorderColor: 'rgba(115,119,191,1)',
+            pointBorderWidth: 2,
             borderWidth: 1
         }]
     },
     options: {
+        legend: {
+            display: false
+        },
         scales: {
             yAxes: [{
                 ticks: {
-                    beginAtZero: true
+                    max: 2500,
+                    stepSize: 500,
+                    beginAtZero: true,
                 }
+            }],
+            xAxes: [{
+                labels: ['wk 1', 'wk 2', 'wk 3', 'wk 4', 'wk 5', 'wk 6', 'wk 7', 'wk 8', 'wk 9', 'wk 10', 'wk 11', 'wk 12']
             }]
         }
     }
+});
+
+//--------------- BAR CHART ---------------//
+//------------------------------------------//
+var barChart = new Chart(trafficBarChart, {
+    type: 'bar',
+    data: {
+        datasets: [{
+            data: [75, 100, 175, 125, 225, 200, 100],
+            backgroundColor: 'rgba(115,119,191,1)',
+        }]
+    },
+    options: {
+        legend: {
+            display: false
+        },
+        scales: {
+            yAxes: [{
+                ticks: {
+                    max: 250,
+                    stepSize: 50,
+                    beginAtZero: true,
+                }
+            }],
+            xAxes: [{
+                labels: ['S', 'M', 'T', 'W', 'T', 'F']
+            }]
+        }
+    }
+});
+
+//--------------- DOUGHNUT CHART ---------------//
+//------------------------------------------//
+var doughnutChart = new Chart(mobileUsersChart, {
+    type: 'doughnut',
+    data: data,
+    options: options
 });
